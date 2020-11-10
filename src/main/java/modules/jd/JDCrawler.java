d54f8dff481ca813e70922937ce618cab267abb6
monalisali <monalisali@hotmail.com>
2020-11-10 17:45:41 +0800

    1.更新README文件 2.添加配置项:isToGetJdProductCategory

--- src/main/java/modules/jd/JDCrawler.java
+++ src/main/java/modules/jd/JDCrawler.java
@@ -1,17 +1,12 @@
 package modules.jd;
 
-import dto.JDCategoryDto;
-import modules.zhihu.ZhihuCrawler;
-import utils.ConstantsHelper;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import utils.Helper;
+import java.util.Properties;
 
 public class JDCrawler {
+    private static Properties properties = Helper.GetAppProperties();
     public static void main(String[] args) {
-        JDProduct jdProduct = new JDProduct(true);
+        JDProduct jdProduct = new JDProduct(Boolean.parseBoolean(properties.getProperty("isToGetJdProductCategory")));
         jdProduct.getJDProducts();
         String ss = "ss";
     }
